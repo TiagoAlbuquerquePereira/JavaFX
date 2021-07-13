96e0c0c3df1cf2c5d264d86d5b1582e8f6f4e8e5
TiagoAlbuquerquePereira <68471513+TiagoAlbuquerquePereira@users.noreply.github.com>
2021-07-13 11:21:17 -0300

    Add files via upload
    5

---
+++ JavaFX5/src/gui/ViewController.java
@@ -0,0 +1,37 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+
+	@FXML
+	private TextField txtNumber1;
+
+	@FXML
+	private TextField txtNumber2;
+
+	@FXML
+	private Label labelResult;
+
+	@FXML
+	private Button btSum;
+
+	@FXML
+	public void onBtSumAction() {
+
+		try {
+			Double number1 = Double.parseDouble(txtNumber1.getText());
+			Double number2 = Double.parseDouble(txtNumber2.getText());
+			Double sum = number1 + number2;
+			labelResult.setText(String.format("%.2f", sum));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Erro", null, "Números inválidos", AlertType.ERROR);
+		}
+	}
+
+}
